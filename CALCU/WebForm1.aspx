be7d1acc7dbf5cbe76f64f159acbd1a74b36b897
Gabriel2898 <gabriel2898cc@gmail.com>
2021-04-28 11:42:29 -0500

    se agregaron los archivos

---
+++ CALCU/WebForm1.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CALCU.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+
+        <table class="auto-style1">
+            <tr>
+                <td colspan="4">
+                    <asp:TextBox ID="txtpantalla" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="1" OnClick="Button1_Click" /></td>
+                <td>
+                    <asp:Button ID="Button2" runat="server" Text="2" OnClick="Button2_Click" /></td>
+                <td>
+                    <asp:Button ID="Button3" runat="server" Text="3" OnClick="Button3_Click" /></td>
+                <td>
+                    <asp:Button ID="Button4" runat="server" Text="-" OnClick="Button4_Click" /></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button5" runat="server" Text="4" OnClick="Button5_Click" /></td>
+                <td>
+                    <asp:Button ID="Button6" runat="server" Text="5" OnClick="Button6_Click" /></td>
+                <td>
+                    <asp:Button ID="Button7" runat="server" Text="6" OnClick="Button7_Click" /></td>
+                <td>
+                    <asp:Button ID="Button8" runat="server" Text="*" OnClick="Button8_Click" /></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button9" runat="server" Text="7" OnClick="Button9_Click" /></td>
+                <td>
+                    <asp:Button ID="Button10" runat="server" Text="8" OnClick="Button10_Click" /></td>
+                <td>
+                    <asp:Button ID="Button11" runat="server" Text="9" OnClick="Button11_Click" /></td>
+                <td>
+                    <asp:Button ID="Button12" runat="server" Text="/" OnClick="Button12_Click" /></td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button13" runat="server" Text="." OnClick="Button13_Click" /></td>
+                <td>
+                    <asp:Button ID="Button14" runat="server" Text="0" OnClick="Button14_Click" /></td>
+                <td>
+                    <asp:Button ID="Button15" runat="server" Text="=" OnClick="Button15_Click" /></td>
+                <td>
+                    <asp:Button ID="Button16" runat="server" Text="+" OnClick="Button16_Click" /></td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <asp:Button ID="Button17" runat="server" Text="Limpiar" /></td>
+            </tr>
+        </table>
+        
+
+    </form>
+</body>
+</html>
---
+++ CALCU/WebForm1.aspx.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CALCU
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        double a;
+        double b;
+        double c;
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "1";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "1";
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "2";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "2";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "3";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "3";
+            }
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "4";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "4";
+            }
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "5";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "5";
+            }
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "6";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "6";
+            }
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "7";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "7";
+            }
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "8";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "8";
+            }
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "9";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "9";
+            }
+        }
+
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            if (txtpantalla.Text == "")
+            {
+                txtpantalla.Text = "0";
+            }
+            else
+            {
+                txtpantalla.Text = txtpantalla.Text + "0";
+            }
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.txtpantalla.Text);
+            c = "/";
+            this.txtpantalla.Clear();
+            this.txtpantalla.Focus();
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.txtpantalla.Text);
+            c = "*";
+            this.txtpantalla.Clear();
+            this.txtpantalla.Focus();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.txtpantalla.Text);
+            c = "-";
+            this.txtpantalla.Clear();
+            this.txtpantalla.Focus();
+        }
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.txtpantalla.Text);
+            c = "+";
+            this.txtpantalla.Clear();
+            this.txtpantalla.Focus();
+        }
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            if (this.txtpantalla.Text.Contains('.') == false)
+            {
+                this.txtpantalla.Text = this.txtpantalla.Text + ".";
+            }
+        }
+
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(this.txtpantalla.Text);
+            switch (c)
+            {
+                case "+":
+                    this.txtpantalla.Text = Convert.ToString(b + a);
+                    break;
+
+                case "-":
+                    this.txtpantalla.Text = Convert.ToString(b - a);
+                    break;
+
+                case "*":
+                    this.txtpantalla.Text = Convert.ToString(b * a);
+                    break;
+
+                case "/":
+                    this.txtpantalla.Text = Convert.ToString(b / a);
+                    break;
+            }
+        }
+    }
+}
